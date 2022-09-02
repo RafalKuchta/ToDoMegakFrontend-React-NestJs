8593e9cdbd36e6aa5c3935eb45125bada40f7eb3
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-09-02 12:04:17 +0200

    Cleaned code.

--- src/components/Tasks/Search/Search.tsx
+++ src/components/Tasks/Search/Search.tsx
@@ -1,23 +1,9 @@
-import React, {SyntheticEvent, useContext, useState} from 'react';
-import {useNavigate} from "react-router";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAngleDown} from "@fortawesome/free-solid-svg-icons";
+import React, {useContext} from 'react';
 import {SearchContext} from "../../../context/search.context";
 import './Search.css';
 
 export const Search = ({setIsLogined}: any) => {
     const {search, setSearch} = useContext(SearchContext);
-    const [isActive, setIsActive] = useState(false);
-
-    // const setSearchFromLocalState = (e: SyntheticEvent) => {
-    //     e.preventDefault();
-    //     setSearch(search)
-    // }
-
-    const handleClick = () => {
-        setIsActive(current => !current);
-    };
-
 
     return (
         <div className="wrapper-search">
