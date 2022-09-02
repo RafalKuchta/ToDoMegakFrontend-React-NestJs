3abf2bf606fcdcb2799a20a25150a28eaedb2a51
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-09-02 11:55:51 +0200

    Added done property to props Done component

--- src/components/common/Done/Done.tsx
+++ src/components/common/Done/Done.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import './Done.css';
 import {useNavigate} from "react-router";
 
 interface Props {
     text: string;
     message?: string;
-    to?: string;
+    to?: any;
+    done?: boolean;
 }
 
 export const Done = (props: Props) => {
@@ -14,7 +14,10 @@ export const Done = (props: Props) => {
     return (
         <div className="done">
             <h3>{props.message}</h3>
-            <button className='done-btn' onClick={() => navigate(`${props.to}`, {replace: true})} >{props.text}</button>
+            <button className='done-btn'
+                    onClick={() => ((
+                      navigate(`${props.to}`, {replace: true})
+            ))}>{props.text}</button>
         </div>
     )
 }
