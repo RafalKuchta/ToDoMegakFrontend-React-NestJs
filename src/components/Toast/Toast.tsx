1b220f0e579ef5c009b45029e93ff66c5ccf52d0
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-10-18 15:00:24 +0200

    Cleaned code, moved components to common and views folders.

--- src/components/Toast/Toast.tsx
+++
@@ -1,8 +0,0 @@
-import {toast} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
-export const Toast = (message: string) => {
-    toast.success(`${message}`, {
-        position: toast.POSITION.BOTTOM_RIGHT,
-    });
-}
