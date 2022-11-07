e7b919ad155a40e97822211b7e44e61b6c143576
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-11-07 14:51:55 +0100

    Edited api link

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,3 @@
-export const apiUrl = process.env.REACT_APP_API_URL ?? 'http://localhost:3001';
+export const apiUrl = 'https://nest.networkmanager.pl/app';
+// export const apiUrl = 'http://localhost:3001';
+// export const apiUrl = process.env.REACT_APP_API_URL ?? 'http://localhost:3001';
